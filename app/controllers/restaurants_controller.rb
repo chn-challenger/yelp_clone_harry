61da07b5cb74c1997e072fe39a8f8e1fbcdd030f
chn.challenger <chn.challenger@gmail.com>
2015-10-12 16:40:07 +0100

    Can now update restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-  
+
   def index
     @restaurants = Restaurant.all
   end
@@ -20,5 +20,15 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
   end
-  
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -8,7 +8,7 @@ feature 'restaurants' do
       expect(page).to have_link 'Add a restaurant'
     end
   end
-  
+
   context 'restaurants have been added' do
     before do
       Restaurant.create(name: 'KFC')
@@ -33,7 +33,6 @@ feature 'restaurants' do
   end
 
   context 'viewing restaurants' do
-
     let!(:kfc){Restaurant.create(name:'KFC')}
 
     scenario 'lets a user view a restaurant' do
@@ -42,7 +41,19 @@ feature 'restaurants' do
      expect(page).to have_content 'KFC'
      expect(current_path).to eq "/restaurants/#{kfc.id}"
     end
-
   end
-end
 
+  context 'editing restaurants' do
+    before { Restaurant.create name: 'KFC' }
+
+    scenario 'let a user edit a restaurant' do
+     visit '/restaurants'
+     click_link 'Edit KFC'
+     fill_in 'Name', with: 'Kentucky Fried Chicken'
+     click_button 'Update Restaurant'
+     expect(page).to have_content 'Kentucky Fried Chicken'
+     expect(current_path).to eq '/restaurants'
+    end
+  end
+
+end
